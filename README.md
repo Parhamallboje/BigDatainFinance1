ad7f8a7ff70ec2f7acc0e0e3f37343c56bcdd09a
Parham Allboje <parham.allboje.samami@gmail.com>
2021-03-05 15:54:04 +0000

    refactored a bit

--- README.md
+++ README.md
@@ -1,15 +1,4 @@
 # BigDatainFinance1
 Repo for code for Big Data in Finance 1 (2021) - Imperial College London.
 
-Regressions, Classifications and more.
-Main files are BigData1.ipynb and helper_functs.py
-
-### Installations
-
-Before getting started, make sure you have python 3 on your machine and run within this environment:
-
-```
-$ pip3 install -r requirements.txt
-```
-
-Just install everything in there and you should be good to go. It's just pandas, sci-kit and matplotlib anyways..
+For the data exploration go [here](BigData1.ipynb), for helper function [here](helper_functs.py), for data and problem statement [here](data_sources).
--- helper_functs.py
+++ helper_functs.py
@@ -5,31 +5,42 @@ Helper Functions for Big Data 1.
 import pandas as pd
 import copy
 import numpy as np
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.decomposition import PCA
 
-def create_stock_df(no, sources):
+def create_stock_df(permno:int, sources:list)->pd.DataFrame:
+    """
+    Create a pd.Dataframe given PERMNO and other df columns in a list.
+    """
     new_df = pd.DataFrame(index=sources[0].index)
     for i in sources:
-        if no in i.columns:
-            new_df[i.name] = i[no]
+        if permno in i.columns:
+            new_df[i.name] = i[permno]
     return new_df
 
 
-def get_lags(df, n_lags):
+def get_lags(df:pd.DataFrame, n_lags:int) -> pd.DataFrame:
+    """
+    Adds n lags for every column to a pd.DataFrame.
+    """
     for col_name in df.columns:
         for l in range(1,n_lags+1):
             df[f'{col_name}_LAG_{l}']= df [col_name].shift(l)
     return df
 
-def x_y_split(df):
+def x_y_split(df:pd.DataFrame) -> pd.DataFrame:
+    """
+    Splits pd.DataFrame into known and unknown parts at any point. 
+    """
     x = df.drop(["FLOWS", "RETURNS"], axis=1)
     y = df[['RETURNS']]
     return x,y
 
 
-def extend_variables(no, x_small, returns, flows, pca_n_components=0):
+def extend_variables(
+    no:int, x_small:pd.DataFrame, 
+    returns:pd.DataFrame, flows:pd.DataFrame, 
+    pca_n_components:int=0)-> pd.DataFrame:
     cl_returns = returns.drop([no], axis=1).shift(1)
     cl_flows = flows.drop([no], axis=1).shift(1)
 
@@ -38,7 +49,7 @@ def extend_variables(no, x_small, returns, flows, pca_n_components=0):
         cl_returns.dropna(inplace=True)
         pca_returns = pca.fit_transform(cl_returns)
         principal_returns_df = pd.DataFrame(data=pca_returns, columns=[
-                                            f"PC_{s}_RETURNS_LAG_1" for s in range(pca_n_components)], index=cl_returns.index)
+                                            f"PCA_{s}_RETURNS_LAG_1" for s in range(pca_n_components)], index=cl_returns.index)
 
         cl_flows.dropna(inplace=True)
         pca_flows = pca.fit_transform(cl_flows)
@@ -58,7 +69,7 @@ def extend_variables(no, x_small, returns, flows, pca_n_components=0):
         return x_small
 
 
-def Rolling_ML(window_size, df, model, hyperparameters={}, progession_param=0):
+def Rolling_ML(window_size:int, df:pd.DataFrame, model, hyperparameters={}, progession_param=0):
     df_x, df_y = x_y_split(df)
     stock_ML = copy.deepcopy(df_y)
     stock_ML["PRED"] = None
@@ -120,15 +131,18 @@ def add_features(df):
     return df
 
 
-def get_percentiles(value, tau=0.01):
+def get_threshold(value, tau=0.01):
     if abs(value) > tau:
         return np.sign(value)
     else:
         return 0
 
 
-def make_market_state(series: pd.Series, tau=0.01):
-    forward_sma = series.rolling(5).mean().shift(-4)
-    forward_sma.iloc[-4:] = series.iloc[-4:]
-    market_state = forward_sma.apply(get_percentiles, tau=tau)
+def make_market_state(series: pd.Series, rolling=5, tau=0.01):
+    if rolling !=0:
+        forward_sma = series.rolling(rolling).mean().shift(-(rolling-1))
+        forward_sma.iloc[-(rolling-1):] = series.iloc[-(rolling-1):]
+        market_state = forward_sma.apply(get_threshold, tau=tau)
+    else: 
+        market_state = forward_sma.apply(get_threshold, tau=tau)
     return market_state
